28989bc3dd4431fe1098993ca1c8d5989b3d71e7
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-22 11:13:44 -0600

    test downgrade

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,11 @@ using Test
 
 @testset "UpdateJulia.jl" begin
 
+    update_julia(v"1.2", set_as_default=true)
     update_julia(v"1.0.0")
     update_julia(v"1.0.5")
-    update_julia(v"1.2", set_as_default=true)
     update_julia(v"1.7.0-rc2")
+    update_julia(v"1.7.0-rc1")
     update_julia(v"1.7.0-rc3")
     update_julia(v"1.6.4")
     update_julia(v"1.6.4", set_as_default=true)
