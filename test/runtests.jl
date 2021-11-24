b0fc1c6cfa698b1f811cfd7b658417f07cf1457f
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-24 10:09:59 -0600

    test preserve old versions

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,8 +9,25 @@ using Test
     @test update_julia("1.2", set_as_default=true) == v"1.2.0"
     @test update_julia("1.0.5") == v"1.0.5"
     @test update_julia("1.0.0") == v"1.0.0"
+    global v10_latest = update_julia("1.0")
+    @test v10_latest >= v"1.0.5" && v10_latest < v"1.1.0"
     @test update_julia("1.7.0-rc1") == v"1.7.0-rc1"
     @test update_julia("1.7.0-rc3") == v"1.7.0-rc3"
-    @test update_julia().prerelease == ()
+    global v_latest = update_julia()
+    @test v_latest == VersionNumber(UpdateJulia.latest())
+    @test v_latest >= v"1.6.4" && v_latest.prerelease == ()
 
+end
+
+@testset "Preserve old versions" begin
+    @test_nowarn UpdateJulia.test("julia", UpdateJulia.latest())
+    @test_nowarn UpdateJulia.test("julia-1.4", "1.4.2")
+    @test_nowarn UpdateJulia.test("julia-1.2", "1.2.0")
+    Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.0.4", "1.0.4")
+    Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.0.0", "1.0.0")
+    @test_nowarn UpdateJulia.test("julia-1.0", v10_latest)
+    Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.7.0-rc1", "1.7.0-rc1")
+    Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.7.0-rc3", "1.7.0-rc3")
+    @test_nowarn UpdateJulia.test("julia-1.7", "1.7.0-rc3")
+    @test_nowarn UpdateJulia.test("julia", v_latest)
 end
