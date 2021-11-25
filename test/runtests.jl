93b87aca71949e421d8aa1215db3a465b72ec4de
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-24 22:57:29 -0600

    skip interactive windows installer tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,16 @@ using Test
 
     printstyled("WARNING: this will both install out of date julia versions and change what the command `julia` points to.\n", color=Base.warn_color())
     printstyled("If all goes well, it will finish with the latest stable version of julia installed.\n", color=Base.warn_color())
-    @test update_julia("1.4", set_as_default=true) == v"1.4.2"
-    @test update_julia("1.2", set_as_default=true) == v"1.2.0"
-    @test update_julia("1.0.4") == v"1.0.4"
-    @test update_julia("1.0.0") == v"1.0.0"
-    global v10_latest = update_julia("1.0")
-    @test v10_latest >= v"1.0.5" && v10_latest < v"1.1.0"
+    @static if Sys.iswindows()
+        @test_skip "interactive windows installer"
+    else
+        @test update_julia("1.4", set_as_default=true) == v"1.4.2"
+        @test update_julia("1.2", set_as_default=true) == v"1.2.0"
+        @test update_julia("1.0.4") == v"1.0.4"
+        @test update_julia("1.0.0") == v"1.0.0"
+        global v10_latest = update_julia("1.0")
+        @test v10_latest >= v"1.0.5" && v10_latest < v"1.1.0"
+    end
     @test update_julia("1.7.0-rc1") == v"1.7.0-rc1"
     @test update_julia("1.7.0-rc3") == v"1.7.0-rc3"
     global v_latest = update_julia()
@@ -21,11 +25,15 @@ end
 
 @testset "Preserve old versions" begin
     @test_nowarn UpdateJulia.test("julia", UpdateJulia.latest())
-    @test_nowarn UpdateJulia.test("julia-1.4", "1.4.2")
-    @test_nowarn UpdateJulia.test("julia-1.2", "1.2.0")
-    Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.0.4", "1.0.4")
-    Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.0.0", "1.0.0")
-    @test_nowarn UpdateJulia.test("julia-1.0", v10_latest)
+    @static if Sys.iswindows()
+        @test_skip "interactive windows installer"
+    else
+        @test_nowarn UpdateJulia.test("julia-1.4", "1.4.2")
+        @test_nowarn UpdateJulia.test("julia-1.2", "1.2.0")
+        Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.0.4", "1.0.4")
+        Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.0.0", "1.0.0")
+        @test_nowarn UpdateJulia.test("julia-1.0", v10_latest)
+    end
     Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.7.0-rc1", "1.7.0-rc1")
     Sys.islinux() && @test_nowarn UpdateJulia.test("julia-1.7.0-rc3", "1.7.0-rc3")
     @test_nowarn UpdateJulia.test("julia-1.7", "1.7.0-rc3")
