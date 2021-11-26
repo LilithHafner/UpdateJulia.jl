1f96d429a725cef345f97e1242dbbd9d46e469cd
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-26 09:33:57 -0600

    test fallback when asking for gui

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
     printstyled("If all goes well, it will finish with the latest stable version of julia installed.\n", color=Base.warn_color())
 
     global v_latest = update_julia()
-    @test v_latest == UpdateJulia.latest()
+    @test v_latest == UpdateJulia.latest(prefer_gui = !Sys.iswindows()) # not supported. test fallback.
     @test v_latest >= v"1.6.4" && v_latest.prerelease == ()
 
     @static if Sys.iswindows()
