b45e6e93262006474327567d3b32eb1ccb1a5d66
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-12-07 19:42:48 -0600

    testing: remove set_default & switch from test to version_of

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,8 +30,8 @@ end
     @static if Sys.iswindows()
         @test_skip "interactive windows installer"
     else
-        @test update_julia("1.4", set_default=true) == v"1.4.2" # TODO check that this reports succeeds on all three counts
-        @test update_julia("1.2", set_default=true) == v"1.2.0"
+        @test update_julia("1.4") == v"1.4.2" # TODO check that this reports succeeds on all three counts
+        @test update_julia("1.2") == v"1.2.0"
         @test update_julia("1.0.4") == v"1.0.4"
         @test update_julia("1.0.0") == v"1.0.0"
         global v10_latest = update_julia("1.0")
@@ -39,8 +39,8 @@ end
     end
 
     @test update_julia("1.5.2", prefer_gui = !Sys.iswindows()) == v"1.5.2" # not supported. test fallback.
-    @test update_julia("1.5.3", set_default=true, systemwide=true, verbose=true) == v"1.5.3" # TODO check that this reports succeeds on all three counts
-    @test update_julia("1.5.1", set_default=true, systemwide=false) == v"1.5.1"
+    @test update_julia("1.5.3", systemwide=true, verbose=true) == v"1.5.3" # TODO check that this reports succeeds on all three counts
+    @test update_julia("1.5.1", systemwide=false) == v"1.5.1"
     @elapsed @test update_julia("1.7.0-rc1", dry_run = true) == v"1.7.0-rc1"
     @test .1 > @elapsed @test update_julia("1.7.0-rc1", dry_run = true) == v"1.7.0-rc1"
     @test update_julia("1.7.0-rc1", dry_run = true, verbose = false) == v"1.7.0-rc1"
@@ -58,24 +58,24 @@ end
     @static if Sys.iswindows()
         @test_skip "interactive windows installer & path management"
     else
-        @test UpdateJulia.test("julia", UpdateJulia.latest())
-        @test UpdateJulia.test("julia-1.4", "1.4.2")
-        @test UpdateJulia.test("julia-1.2", "1.2.0")
-        @test UpdateJulia.test("julia-1.0.4", "1.0.4")
-        @test UpdateJulia.test("julia-1.0.0", "1.0.0")
-        @test UpdateJulia.test("julia-1.0", v10_latest)
+        @test UpdateJulia.version_of("julia") == UpdateJulia.latest()
+        @test UpdateJulia.version_of("julia-1.4") == v"1.4.2"
+        @test UpdateJulia.version_of("julia-1.2") == v"1.2.0"
+        @test UpdateJulia.version_of("julia-1.0.4") == v"1.0.4"
+        @test UpdateJulia.version_of("julia-1.0.0") == v"1.0.0"
+        @test UpdateJulia.version_of("julia-1.0") == v10_latest
     end
-    @test UpdateJulia.test("julia-1.5.1", "1.5.1")
-    @test UpdateJulia.test("julia-1.5.2", "1.5.2")
-    @test UpdateJulia.test("julia-1.5.3", "1.5.3")
-    @test UpdateJulia.test("julia-1.7.0-rc1", "1.7.0-rc1")
-    @test UpdateJulia.test("julia-1.7.0-rc3", "1.7.0-rc3")
+    @test UpdateJulia.version_of("julia-1.5.1") == v"1.5.1"
+    @test UpdateJulia.version_of("julia-1.5.2") == v"1.5.2"
+    @test UpdateJulia.version_of("julia-1.5.3") == v"1.5.3"
+    @test UpdateJulia.version_of("julia-1.7.0-rc1") == v"1.7.0-rc1"
+    @test UpdateJulia.version_of("julia-1.7.0-rc3") == v"1.7.0-rc3"
     @static if Sys.iswindows()
         @test_skip "path management"
     else
-        @test_skip UpdateJulia.test("julia-1.7", "1.7.0-rc3") # Depends on what the latest 1.7 release is
-        @test_skip UpdateJulia.test("julia-1.5", "1.5.1") # Could be 1.5.1 or 1.5.3 depending on path order b.c. of coexisting systemwide and user installations
+        @test_skip UpdateJulia.version_of("julia-1.7") == v"1.7.0-rc3" # Depends on what the latest 1.7 release is
+        @test_skip UpdateJulia.version_of("julia-1.5") == v"1.5.1" # Could be 1.5.1 or 1.5.3 depending on path order b.c. of coexisting systemwide and user installations
         @test update_julia("1.5") >= v"1.5.3"
-        @test UpdateJulia.test("julia", v_latest)
+        @test UpdateJulia.version_of("julia") == v_latest
     end
 end
