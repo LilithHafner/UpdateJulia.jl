f9ad543e5d24258bd9a3c6f754a7a75a735b5986
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-12-09 14:19:25 -0600

    skip slow tests except on CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -54,104 +54,110 @@ end
         UpdateJulia.nightly_version[]
 end
 
-@testset "quick test" begin
-    @test update_julia() == UpdateJulia.latest()
-    v_nightly = update_julia("nightly")
-    @test v_nightly >= v"1.8-DEV" && v_nightly > UpdateJulia.latest() && v_nightly.prerelease == ("DEV",)
-end
+if ("CI" => "true") ∈ ENV
 
-#@testset "randomized tests" begin
+    @testset "quick test" begin
+        @test update_julia() == UpdateJulia.latest()
+        v_nightly = update_julia("nightly")
+        @test v_nightly >= v"1.8-DEV" && v_nightly > UpdateJulia.latest() && v_nightly.prerelease == ("DEV",)
+    end
 
-    printstyled("WARNING: this will both install out of date julia versions and change what the command `julia` points to.\n", color=Base.warn_color())
-    printstyled("If all goes well, it will finish with the latest stable version of julia installed.\n", color=Base.warn_color())
+    #@testset "randomized tests" begin
 
-    versions = vcat(UpdateJulia.nightly_version[], filter!(collect(keys(UpdateJulia.versions[]))) do x
-        x >= (Sys.iswindows() ? v"1.5.0-rc2" : v"1.0.0")
-    end)
-    keywords = [
-        # os_str => untested
-        # arch => untested
-        # prefer_gui => untested
-        :fetch => Bool,
-        # _v_url => untested
-        # v => untested
-        # url => untested
-        # aliases => untested
-        :systemwide => Bool,
-        :install_location => [mktempdir(), mktempdir()], # Tuple sampling is missing in julia 1.0
-        # bin => untested
-        :dry_run => Bool,
-        :verbose => Bool]
+        printstyled("WARNING: this will both install out of date julia versions and change what the command `julia` points to.\n", color=Base.warn_color())
+        printstyled("If all goes well, it will finish with the latest stable version of julia installed.\n", color=Base.warn_color())
 
-    installed_versions = [UpdateJulia.latest(), UpdateJulia.nightly_version[]]
-    #@testset "install" begin
-    #    Random.seed!()
-        for _ in 1:10
-            v = rand(versions)
-            version = rand(["", "$(v.major)", "$(v.major).$(v.minor)", "$(v.major).$(v.minor).$(v.patch)", "$v"])
-            kw = [k=>rand(source) for (k, source) in filter(x->rand(Bool), keywords)]
-            v_inst = update_julia(version; kw...)
-            @test v_inst === v || UpdateJulia.prefer(v_inst, v)
-            if (:dry_run => true) ∉ kw
-                push!(installed_versions, v_inst)
-            end
-        end
-    #end
+        versions = vcat(UpdateJulia.nightly_version[], filter!(collect(keys(UpdateJulia.versions[]))) do x
+            x >= (Sys.iswindows() ? v"1.5.0-rc2" : v"1.0.0")
+        end)
+        keywords = [
+            # os_str => untested
+            # arch => untested
+            # prefer_gui => untested
+            :fetch => Bool,
+            # _v_url => untested
+            # v => untested
+            # url => untested
+            # aliases => untested
+            :systemwide => Bool,
+            :install_location => [mktempdir(), mktempdir()], # Tuple sampling is missing in julia 1.0
+            # bin => untested
+            :dry_run => Bool,
+            :verbose => Bool]
 
-    commands = unique(vcat("julia", [["julia-$(v.major).$(v.minor)", "julia-$v"]
-        for v in vcat(UpdateJulia.nightly_version[], versions)]...))
-    @test String["julia", "julia-1.8", "julia-1.5.1", "julia-1.6.2", "julia-1.5.0-rc2",
-        "julia-1.7.0-rc1", "julia-1.7.0-beta2"] ⊊ commands
-    Sys.iswindows() || @test String["julia-1.0", "julia-1.4",  "julia-1.3.1",
-        "julia-1.3.0-rc3", "julia-1.3.0-alpha"] ⊊ commands
-
-
-    @testset "check" begin
-        for c in commands
-            installed = filter(x->startswith(string(x), c[7:end]), installed_versions)
-            actual = UpdateJulia.version_of(c)
-            for i in installed
-                if UpdateJulia.prefer(i, actual)
-                    println("Running: $c")
-                    println("Gives result: $actual")
-                    println("Installed these compatible versions, in this order: $installed")
-                    println("Specifically, $i, which is better than $actual")
-                    @test false
-                else
-                    @test true
+        installed_versions = [UpdateJulia.latest(), UpdateJulia.nightly_version[]]
+        #@testset "install" begin
+        #    Random.seed!()
+            for _ in 1:10
+                v = rand(versions)
+                version = rand(["", "$(v.major)", "$(v.major).$(v.minor)", "$(v.major).$(v.minor).$(v.patch)", "$v"])
+                kw = [k=>rand(source) for (k, source) in filter(x->rand(Bool), keywords)]
+                v_inst = update_julia(version; kw...)
+                @test v_inst === v || UpdateJulia.prefer(v_inst, v)
+                if (:dry_run => true) ∉ kw
+                    push!(installed_versions, v_inst)
                 end
             end
-            if !ismissing(actual) && actual ∉ installed
-                @warn "Unexpectedly found julia version $actual when running `$c -v`. Only installed $installed in testing."
+        #end
+
+        commands = unique(vcat("julia", [["julia-$(v.major).$(v.minor)", "julia-$v"]
+            for v in vcat(UpdateJulia.nightly_version[], versions)]...))
+        @test String["julia", "julia-1.8", "julia-1.5.1", "julia-1.6.2", "julia-1.5.0-rc2",
+            "julia-1.7.0-rc1", "julia-1.7.0-beta2"] ⊊ commands
+        Sys.iswindows() || @test String["julia-1.0", "julia-1.4",  "julia-1.3.1",
+            "julia-1.3.0-rc3", "julia-1.3.0-alpha"] ⊊ commands
+
+
+        @testset "check" begin
+            for c in commands
+                installed = filter(x->startswith(string(x), c[7:end]), installed_versions)
+                actual = UpdateJulia.version_of(c)
+                for i in installed
+                    if UpdateJulia.prefer(i, actual)
+                        println("Running: $c")
+                        println("Gives result: $actual")
+                        println("Installed these compatible versions, in this order: $installed")
+                        println("Specifically, $i, which is better than $actual")
+                        @test false
+                    else
+                        @test true
+                    end
+                end
+                if !ismissing(actual) && actual ∉ installed
+                    @warn "Unexpectedly found julia version $actual when running `$c -v`. Only installed $installed in testing."
+                end
             end
         end
-    end
 
-    @static if Sys.iswindows()
-        @test_skip "interactive windows installer"
-    else
-        @test update_julia("1.4") == v"1.4.2" # TODO check that this reports succeeds on all three counts
-        @test update_julia("1.2") == v"1.2.0"
-        @test update_julia("1.0.4") == v"1.0.4"
-        global v10_latest = update_julia("1.0")
-        @test v10_latest >= v"1.0.5" && v10_latest < v"1.1.0"
-        @test update_julia("1.0.0") == v"1.0.0"
-    end
+        @static if Sys.iswindows()
+            @test_skip "interactive windows installer"
+        else
+            @test update_julia("1.4") == v"1.4.2" # TODO check that this reports succeeds on all three counts
+            @test update_julia("1.2") == v"1.2.0"
+            @test update_julia("1.0.4") == v"1.0.4"
+            global v10_latest = update_julia("1.0")
+            @test v10_latest >= v"1.0.5" && v10_latest < v"1.1.0"
+            @test update_julia("1.0.0") == v"1.0.0"
+        end
 
-    #Dry run
-    f() = @test @suppress update_julia("1.7.0-rc1", dry_run=true, verbose=false) == v"1.7.0-rc1"
-    @elapsed f()
-    @test .05 > @elapsed f()
+        #Dry run
+        f() = @test @suppress update_julia("1.7.0-rc1", dry_run=true, verbose=false) == v"1.7.0-rc1"
+        @elapsed f()
+        @test .05 > @elapsed f()
 
-    #Force fetch
-    t_before_force_fetch = UpdateJulia.last_fetched[]
-    @test update_julia("1.7.0-rc3", fetch = true) == v"1.7.0-rc3"
-    t_after_force_fetch = UpdateJulia.last_fetched[]
-    @test 0 < t_before_force_fetch < t_after_force_fetch < time()
+        #Force fetch
+        t_before_force_fetch = UpdateJulia.last_fetched[]
+        @test update_julia("1.7.0-rc3", fetch = true) == v"1.7.0-rc3"
+        t_after_force_fetch = UpdateJulia.last_fetched[]
+        @test 0 < t_before_force_fetch < t_after_force_fetch < time()
 
-    #Finish with latest
-    @test update_julia() == UpdateJulia.latest()
-#end
+        #Finish with latest
+        @test update_julia() == UpdateJulia.latest()
+    #end
+
+else
+    @warn "Skipped installation tests!"
+end
 
 @testset "report path" begin
     println(ENV["PATH"])
