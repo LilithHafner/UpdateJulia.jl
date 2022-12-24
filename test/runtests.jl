edea213ea90b35b6fbbaa397176896f99b99e51d
Lilith Orion Hafner <lilithhafner@gmail.com>
2022-12-24 11:04:41 -0600

    Make `update_julia(1.1)` install 1.1, not 1.1x (e.g. 1.10) (#21)
    This predates the release of 1.9 so it's a bit early but tests are sensitive enough to start detecting this failure now that nightly is 1.10-DEV

--- test/runtests.jl
+++ test/runtests.jl
@@ -38,9 +38,19 @@ end
     @test UpdateJulia.prefer(v"1.7.0-rc1", missing)
 end
 
+@testset "isconsistent()" begin
+    @test !UpdateJulia.isconsistent("1.10.0-DEV.1729", "1.1")
+    @test UpdateJulia.isconsistent("1.10.0-DEV.1729", "1.10")
+    @test UpdateJulia.isconsistent("1.10.0-DEV.1729", "")
+    @test UpdateJulia.isconsistent("3.0.6", "3.0")
+    @test UpdateJulia.isconsistent("3.0.6", "3.0.6")
+    @test !UpdateJulia.isconsistent("3.0.62", "3.0.6")
+end
+
 @testset "latest()" begin
     @test UpdateJulia.latest() >= v"1.7.0"
     @test UpdateJulia.latest().prerelease == ()
+    @test UpdateJulia.latest("1.1") == v"1.1.1"
     @test UpdateJulia.latest("1.1.") == v"1.1.1"
     @test UpdateJulia.latest("1.5") == v"1.5.4"
     @test UpdateJulia.latest("1.2") == v"1.2.0"
@@ -159,7 +169,7 @@ function random_matrix_test(n)
                 # If inexact, and cvs is a complete version string, don't match
                 try string(VersionNumber(cvs)) == cvs && return false catch end
                 # Otherwise match if prefix
-                startswith(xs, cvs)
+                UpdateJulia.isconsistent(xs, cvs)
             end
             actual = UpdateJulia.version_of(c)
             for i in installed
