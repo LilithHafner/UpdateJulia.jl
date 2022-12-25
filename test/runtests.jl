5e60277ee0cbe9e22ba43bf7f99ae8c60376ae81
Lilith Orion Hafner <lilithhafner@gmail.com>
2022-12-25 07:45:03 -0600

    Fix typo in comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -135,7 +135,7 @@ function random_matrix_test(n)
                 if x isa ProcessFailedException && ((:migrate_packages => :force) ∈ kw || (:migrate_packages => true) ∈ kw)
                     v = UpdateJulia.latest(version)
                     if (v.major, v.minor) < (VERSION.major, VERSION.minor)
-                        # Package migratoin requires Pkg at version to be compatible with
+                        # Package migration requires Pkg at version to be compatible with
                         # VERSION's Project.toml. This is not required by SymVer, and not
                         # the case for 1.0's Pkg with 1.8's Project.toml. So, if we
                         # explicitly ask for package migration in a regression (even without
